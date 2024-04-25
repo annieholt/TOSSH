a1a6f2e325e13b90fe980b79910dfb1908b661ec
annieholt <holtannie@gmail.com>
2024-04-24 18:38:43 -0700

    exporting streamflow and precip data that has been prepped

--- example/CAMELS_groundwater_2.m
+++ example/CAMELS_groundwater_2.m
@@ -4,13 +4,13 @@ CAMELS_data_all = loadCAMELSstruct();
 CAMELS_data = CAMELS_dataprep();
 n_CAMELS = 671;
 
-% Define the directory
-export_path = 'E:\SDSU_GEOG\Thesis\Data\Signatures\camels_data_prepped'; 
-
- % Create the directory to store the files if it doesn't exist
- if ~exist(export_path, 'dir')
-     mkdir(export_path);
- end
+% % Define the output directory
+% export_path = 'E:\SDSU_GEOG\Thesis\Data\Signatures\camels_data_prepped\precipitation_1989_2009'; 
+% 
+%  % Create the directory to store the files if it doesn't exist
+%  if ~exist(export_path, 'dir')
+%      mkdir(export_path);
+%  end
 
 for i = 1:n_CAMELS
     Q_mat = {CAMELS_data.Q_mat{1,i}};
@@ -19,10 +19,11 @@ for i = 1:n_CAMELS
     PET_mat = {CAMELS_data.PET_mat{1,i}};
     
     % Extract numeric array from the cell for exporting also
-    Q_mat_num = Q_mat{1};
+    % Q_mat_num = Q_mat{1};
+    % P_mat_num = P_mat{1};
 
     gauge_id = CAMELS_data_all.gauge_id(i,1);
-    gauge_id_out = num2str(gauge_id); % Convert gauge_id to string
+    % gauge_id_out = num2str(gauge_id); % Convert gauge_id to string
     gauge_lat = CAMELS_data_all.gauge_lat(i,1);
     gauge_lon = CAMELS_data_all.gauge_lon(i,1);
     
@@ -31,9 +32,9 @@ for i = 1:n_CAMELS
     results.gauge_lat{i} = gauge_lat;
     results.gauge_lon{i} = gauge_lon;
 
-    % Write Q_mat data to a CSV file named by gauge_id
-    filename = fullfile(export_path, sprintf('%s.csv', gauge_id_out));
-    writematrix(Q_mat_num, filename);
+    % % Write Q_mat data to a CSV file named by gauge_id
+    % filename = fullfile(export_path, sprintf('%s.csv', gauge_id_out));
+    % writematrix(P_mat_num, filename);
 
  end
     
